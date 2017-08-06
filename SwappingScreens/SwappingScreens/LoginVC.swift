e91189884a69fcd939945d284c1695b57e3e58d4
Tom <munhoven@gmail.com>
2017-08-06 18:01:15 +0200

    Final version ( w/o comments )

---
+++ SwappingScreens/SwappingScreens/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Tom on 06/08/2017.
+//  Copyright © 2017 Tom. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+
+}
+
---
+++ SwappingScreens/SwappingScreens/MusicListVC.swift
@@ -0,0 +1,46 @@
+//
+//  MusicListVC.swift
+//  SwappingScreens
+//
+//  Created by Tom on 06/08/2017.
+//  Copyright © 2017 Tom. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.blue
+        
+    }
+    
+    @IBAction func backBTNPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func load3rdScreenPressed(_ sender: Any) {
+        
+        let songTitle = "Quit Playing Games with my hearts"
+        
+        performSegue(withIdentifier: "PlaySongVC", sender: songTitle)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if let destination = segue.destination as? PlaySongVC {
+            
+            if let song = sender as? String {
+                destination.selectedSong = song
+            }
+            
+        }
+    }
+    
+}
